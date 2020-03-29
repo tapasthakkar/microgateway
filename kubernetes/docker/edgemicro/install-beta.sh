04b2cd5ef570ed401f8e4f03fb1941440a7874b8
Keyur Karnik <keyur.karnik+github@gmail.com>
2020-03-28 20:15:00 -0700

    Update docker image to remove alpine
    Node alpine image contains busybox and may cause conflicts.
    Moving to debian:buster-slim based node image

--- kubernetes/docker/edgemicro/install-beta.sh
+++ kubernetes/docker/edgemicro/install-beta.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set echo off
 
@@ -11,7 +11,7 @@ chown apigee:apigee /opt/apigee && \
     mkdir /opt/apigee/.edgemicro && \
     chown apigee:apigee /opt/apigee/.edgemicro && \
     ln -s /opt/apigee/.edgemicro /root/.edgemicro && \
-    su - apigee -s /bin/sh -c "edgemicro init" && \
+    su - apigee -s /bin/bash -c "edgemicro init" && \
     chmod +x /opt/apigee/entrypoint.sh && \
     chown apigee:apigee /opt/apigee/entrypoint.sh && \
     ln -s /opt/apigee/entrypoint.sh /usr/local/bin/entrypoint
