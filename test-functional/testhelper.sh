ec7a3d8afbdef1e5536b544639ec8f028e3dda4e
Nitesh Gupta <guptanitesh40@gmail.com>
2025-08-11 18:23:06 +0530

    119788173 Add comprehensive automation suite and perform cleanup
     - Replaces the outdated test suite with a full end-to-end automation suite.
     - Removes all identified obsolete and duplicated tests.

--- test-functional/testhelper.sh
+++ test-functional/testhelper.sh
@@ -29,7 +29,7 @@ function listDevelopers() {
     local ret=0
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/developers" 
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -D headers.txt -o listDevelopers.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -D headers.txt -o listDevelopers.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully retrieved developer list with code $result"
@@ -56,7 +56,7 @@ function createDeveloper() {
 
     node substVars "templates/apideveloper-template.json" "devguy" "${apiDeveloper}" > "${apiDeveloper}".json
 
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -X POST -d @"${apiDeveloper}".json -D headers.txt -o createDeveloper.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -X POST -d @"${apiDeveloper}".json -D headers.txt -o createDeveloper.txt > /dev/null 2>&1 ; ret=$?
 
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 201 ]; then
@@ -82,7 +82,7 @@ function listDeveloper() {
     apiDeveloper="$1"
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/developers/${apiDeveloper}@google.com"
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -D headers.txt -o listDeveloper.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -D headers.txt -o listDeveloper.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully retrieved developer with code $result"
@@ -106,7 +106,7 @@ function deleteDeveloper() {
     apiDeveloper="$1"
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/developers/${apiDeveloper}@google.com"
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" -X DELETE ${apiProxyURL} -D headers.txt -o deleteDeveloper.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" -X DELETE ${apiProxyURL} -D headers.txt -o deleteDeveloper.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully deleted developer with code $result"
@@ -136,7 +136,7 @@ function listDeveloperApps() {
     apiDeveloper="$1"
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/developers/${apiDeveloper}@google.com/apps"
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -D headers.txt -o listDeveloperApps.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -D headers.txt -o listDeveloperApps.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully retrieved developer app list with code $result"
@@ -165,7 +165,7 @@ function createDeveloperApp() {
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/developers/${apiDeveloper}@google.com/apps" 
 
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -X POST -d @${apiDeveloperApp}.json -D headers.txt -o createDeveloperApp.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -X POST -d @${apiDeveloperApp}.json -D headers.txt -o createDeveloperApp.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 201 ]; then
          logInfo "Successfully created developer app with code $result"
@@ -190,7 +190,7 @@ function listDeveloperApp() {
     apiDeveloperApp="$2"
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/developers/${apiDeveloper}@google.com/apps/${apiDeveloperApp}"
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -D headers.txt -o listDeveloperApp.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -D headers.txt -o listDeveloperApp.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully retrieved developer app with code $result"
@@ -216,7 +216,7 @@ function getDeveloperApiKey() {
     apiDeveloperApp="$2"
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/developers/${apiDeveloper}@google.com/apps/${apiDeveloperApp}"
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -D headers.txt -o getDeveloperApiKey.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -D headers.txt -o getDeveloperApiKey.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully retrieved developer api key with code $result"
@@ -243,7 +243,7 @@ function deleteDeveloperApp() {
     apiDeveloperApp="$2"
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/developers/${apiDeveloper}@google.com/apps/${apiDeveloperApp}"
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" -X DELETE ${apiProxyURL} -D headers.txt -o deleteDeveloperApp.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" -X DELETE ${apiProxyURL} -D headers.txt -o deleteDeveloperApp.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully delete developer app with code $result"
@@ -273,7 +273,7 @@ function listAPIProxy() {
     apiProxyName="$1"
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/apis/${apiProxyName}"
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -D headers.txt -o listAPIProxy.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -D headers.txt -o listAPIProxy.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully retrieve API Proxy list with code $result"
@@ -297,7 +297,7 @@ function listAPIProxies() {
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/apis" 
 
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -D headers.txt -o listAPIProxies.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN"  ${apiProxyURL} -D headers.txt -o listAPIProxies.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully retrieve API Proxies list with code $result"
@@ -322,7 +322,7 @@ function createAPIProxy() {
 
     node substVars "templates/apiproxy-template.json" "proxyName" "$apiProxyName" > "${apiProxyName}".json
 
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -X POST -d @"${apiProxyName}".json -D headers.txt -o createAPIProxy.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN"  ${apiProxyURL} -X POST -d @"${apiProxyName}".json -D headers.txt -o createAPIProxy.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 201 ]; then
          logInfo "Successfully created API Proxy with code $result"
@@ -347,7 +347,7 @@ function listAPIProxy() {
     apiProxyName="$1"
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/apis/${apiProxyName}"
 
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -D headers.txt -o listAPIProxy.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -D headers.txt -o listAPIProxy.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully retrieve API Proxy list with code $result"
@@ -374,7 +374,7 @@ function updateAPIProxy() {
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/apis/${apiProxyName}/revisions/${apiProxyBundleRevision}" 
 
-    ${CURL} -H "Content-Type: multipart/form-data" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -X POST -F "file=@${apiProxyBundle}" -D headers.txt -o updateAPIProxy.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type: multipart/form-data" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN"  ${apiProxyURL} -X POST -F "file=@${apiProxyBundle}" -D headers.txt -o updateAPIProxy.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | grep -v 100 | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully updated API Proxy with code $result"
@@ -401,7 +401,7 @@ function deployAPIProxy() {
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/environments/${envName}/apis/${apiProxyName}/revisions/${apiProxyBundleRevision}/deployments" 
 
-    ${CURL} -H "Content-Type: application/x-www-form-urlencoded" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -X POST -D headers.txt -o deployAPIProxy.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -X POST -D headers.txt -o deployAPIProxy.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully deployed API Proxy with code $result"
@@ -428,7 +428,7 @@ function undeployAPIProxy() {
 
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/environments/${envName}/apis/${apiProxyName}/revisions/${apiProxyBundleRevision}/deployments" 
 
-    ${CURL} -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -X DELETE -D headers.txt -o undeployAPIProxy.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -X DELETE -D headers.txt -o undeployAPIProxy.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully undeployed API Proxy with code $result"
@@ -452,7 +452,7 @@ function deleteAPIProxy() {
     apiProxyName="$1"
     apiProxyURL="${API_PROXY_URL}/${MOCHA_ORG}/apis/${apiProxyName}"
 
-    ${CURL} -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${apiProxyURL} -X DELETE -D headers.txt -o deleteAPIProxy.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${apiProxyURL} -X DELETE -D headers.txt -o deleteAPIProxy.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully deleted API Proxy with code $result"
@@ -514,7 +514,7 @@ function listAPIProducts() {
     local ret=0
 
     productURL="${API_PRODUCT_URL}/${MOCHA_ORG}/apiproducts"
-    ${CURL} -q -s -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${productURL} -D headers.txt -o listAPIProducts.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -q -s -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${productURL} -D headers.txt -o listAPIProducts.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully retrieved API Products list with code $result"
@@ -542,7 +542,7 @@ function createAPIProduct() {
 
     productURL="${API_PRODUCT_URL}/${MOCHA_ORG}/apiproducts"
 
-    ${CURL} -q -s -H "Content-Type:application/json" -X POST -d @"${apiProductName}".json -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${productURL} -D headers.txt -o createAPIProduct.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -q -s -H "Content-Type:application/json" -X POST -d @"${apiProductName}".json -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${productURL} -D headers.txt -o createAPIProduct.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 201 ]; then
          logInfo "Successfully created API Product with code $result"
@@ -566,7 +566,7 @@ function listAPIProduct() {
     apiProductName="$1"
 
     productURL="${API_PRODUCT_URL}/${MOCHA_ORG}/apiproducts/${apiProductName}"
-    ${CURL} -q -s -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${productURL} -D headers.txt -o listAPIProduct.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -q -s -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${productURL} -D headers.txt -o listAPIProduct.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully retrieved API Product list with code $result"
@@ -590,7 +590,7 @@ function deleteAPIProduct() {
     apiProductName="$1"
     productURL="${API_PRODUCT_URL}/${MOCHA_ORG}/apiproducts/${apiProductName}"
 
-    ${CURL} -q -s -H "Content-Type:application/json" -u "$MOCHA_USER":"$MOCHA_PASSWORD" ${productURL} -X DELETE -D headers.txt -o deleteAPIProduct.txt > /dev/null 2>&1 ; ret=$?
+    ${CURL} -q -s -H "Content-Type:application/json" -H "Authorization: Bearer $MOCHA_BEARER_TOKEN" ${productURL} -X DELETE -D headers.txt -o deleteAPIProduct.txt > /dev/null 2>&1 ; ret=$?
     result=$(grep HTTP headers.txt | cut -d ' ' -f2)
     if [ ${ret} -eq 0 -a ${result} -eq 200 ]; then
          logInfo "Successfully deleted API Product with code $result"
